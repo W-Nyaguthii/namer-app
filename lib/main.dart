dee9793ed312f1f607c0cb462011f67f1e415ec8
P2Wambui <wambuiprimrose0@gmail.com>
2025-02-14 08:29:32 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+//import 'package:bloc/bloc.dart';
+import 'package:namer_app/app.dart';
+//import 'package:firebase_core/firebase_core.dart';
+//import 'simple_bloc_observer.dart';
+
+void main() {
+  runApp(const MyApp());
+}
