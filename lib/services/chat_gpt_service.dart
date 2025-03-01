942835b2e39f84f80963a2688a896190d16c4e13
P2Wambui <wambuiprimrose0@gmail.com>
2025-03-02 00:42:24 +0300

    Added budget & savings screens

--- lib/services/chat_gpt_service.dart
+++ lib/services/chat_gpt_service.dart
@@ -2,8 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ChatGPTService {
-  final String apiKey =
-      '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'; // Replace with your actual API key
+  final String apiKey = ''; // Replace with your actual API key
   final String apiUrl = 'https://api.openai.com/v1/chat/completions';
 
   Future<String> sendMessage(String message) async {
